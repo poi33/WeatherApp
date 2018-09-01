fb326df825c10464db1a536060c251bad1608e5a
Persijn <persijn.d.k@gmail.com>
2018-09-01 22:22:24 +0200

    Improved browser support

--- JS/WeatherData.js
+++ JS/WeatherData.js
@@ -1,87 +1,99 @@
 /* Script to call the weather api data */
 /* Api used: https://openweathermap.org/current */
 
+$.support.cors = true;
+
 var imgSrc = {
-    "clear sky": "./Images/Sunny.jpg"
-    //TODO THE REST!
-}
+    "clear sky": "./Images/Sunny.jpg",
+    "few clouds" : "./Images/SunCloud.jpg",
+    "scattered clouds" : "./Images/SunCloud.jpg",
+    "broken clouds" : "./Images/DarkCloud.jpg",
+    "shower rain" : "./Images/RainLight.jpg",
+    "rain" : "./Images/SunnyCloudRain.jpg",
+    "thunderstorm" : ".Images/Storm.jpg",
+    "snow" : "./Images/Snow.jpg"
+};
 
 var dontUpdate = false;
 
 /* Run on page load */
 $(function () {
-    SetWeatherData()
+    SetWeatherData();
 
-    var minUpdate
-    minUpdate = setInterval(function() {
+    var minUpdate;
+    minUpdate = setInterval(function () {
         if (dontUpdate === true) {
             clearInterval(minUpdate);
-            return 
+            return;
         }
-        console.log("Update");
-        SetWeatherData()
-    }, 1000*30) 
-})
+        SetWeatherData();
+    }, 1000 * 30);
+});
 
 /* Search weathercontent blockid. Creates a new one if it cant find it */
 function UpdateBlock(data, index) {
-    let container = $("#WeatherContent")
-    let block
+    var container = $("#WeatherContent");
+    var block;
     if (container.children("#Block" + index).length > 0) {
-        block = container.find("#Block" + index)
+        block = container.find("#Block" + index);
     } else {
-        block = $("#Templates").children(".WeatherBlock").clone(true, true)
-        block.attr("id", "Block"+index);
-        container.append(block)
+        block = $("#Templates").children(".WeatherBlock").clone(true, true);
+        block.attr("id", "Block" + index);
+        container.append(block);
     }
     //Not a fan of hooking onto a description.
-    let image = imgSrc[data.weather[0].description]
+    var image = imgSrc[data.weather[0].description];
 
-    block.find(".City h2").html(data.name)
-    block.find(".Icon img").attr("src", image);
-    block.find(".Temperature").html(data.main.temp + "&deg;")
-    block.find(".Icon figcaption").html(data.weather[0].description)
-
-    
+    block.find(".City h2").html(data.name);
+    block.find(".City .WeatherIcon").attr({
+        "src": image,
+        "alt": data.weather[0].description
+    });
+    block.find(".City .Temperature").html(data.main.temp + " &deg;C");
+    block.find(".City .Wind").html(data.wind.speed+" m/s");
 }
 
 /* Gets the current weather with an api call
  * and sends the data to an update */
 function SetWeatherData() {
-    let londonId = 2643743
-    let osloId = 6453366
-    let minskId = 625144
+    var londonId = 2643743;
+    var osloId = 6453366;
+    var minskId = 625144;
 
-    let apiKey = "f2bc5fd358ea3bd542e898f165275606"
-    let reqString = "https://api.openweathermap.org/data/2.5/"
+    var apiKey = "f2bc5fd358ea3bd542e898f165275606";
+    var reqString = "https://api.openweathermap.org/data/2.5/"
         + "group?id=" + londonId + "," + osloId + "," + minskId
         + "&units=metric"
-        + "&APPID=" + apiKey
+        + "&APPID=" + apiKey;
 
     $.getJSON(reqString)
         .done(function (data) {
             var size = data.cnt;
 
-            let allCities = data.list
+            var allCities = data.list;
             for (var i = 0; i < size; i++) {
-                cityData = allCities[i]
-                UpdateBlock(cityData, i)
+                cityData = allCities[i];
+                UpdateBlock(cityData, i);
             }
 
             updateFetchTime();
-            let loading = $(".Main .Headline .Loading")
+            var loading = $(".Main .Headline .Loading");
             if (loading.hasClass("Hidden") == false) {
                 loading.toggleClass("Hidden");
             }
         })
         .fail(function (data, textStatus, error) {
-            let message = "GetJson failed. Status" + textStatus + " Error " + error
-            console.log(message)
-            $(".Headline").addClass("error").html("Error: Api request failed")
+            var message = "GetJson failed. Status" + textStatus + " Error " + error;
+            console.log(message);
+            dontUpdate = true;
+            $(".Headline").addClass("Error").html("Error: Api request failed");
         })
 }
 
 function updateFetchTime() {
-    let time = moment();
-    $("#FetchTime").html(time.format("HH:mm:ss DD.MM.YY"))
+    var time = moment();
+    $("#FetchTime").html(
+        time.format("HH:mm:ss") + " on " + 
+        time.format("DD.MM.YY")
+    );
 }
